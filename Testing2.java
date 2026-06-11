5a548fc401abb4a351679847e95e840ca48e6c32
Biswajeet Mishra <bmishra@l6jx1qy3-ban.int.openbet.com>
2026-06-11 20:24:59 +0530

    adding new file

---
+++ Testing2.java
@@ -0,0 +1,14 @@
+class Testing2
+{
+    static void add(int a, int b)
+    {
+        int c = a + b;
+        System.out.println("Addition is " + c);
+    }
+
+    public static void main(String[] args)
+    {
+        B.add(5, 10);
+    }
+}
+
